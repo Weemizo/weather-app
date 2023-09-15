a495c18cb6d9695705c0c1d0df2900cd6b9d1d12
weemizo <mrceleius@gmail.com>
2023-09-15 18:26:33 +0200

    1

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -4,10 +4,20 @@ function App() {
 
   return (
     <>
+      <main>
         <div className='navbar'>
           Weather App
         </div>
-        
+
+        <div className='contents'>
+
+            <div className='search'>
+              <input type="text" placeholder='Search' className='searchBar'/> <button type="submit"> </button> 
+            </div>
+
+        </div>
+
+      </main>
         {/* https://openweathermap.org/api <<< z tego api bedziemy fetchowac dane do pogody */}
     </>
   )
